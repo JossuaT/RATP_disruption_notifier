04dd74f2757c11417b3354b5c548a2841811cc34
jossua-ws <jossua.thuiller@wavestone.com>
2026-03-02 14:40:10 +0000

    created notifier system

---
+++ src/project/notifier/notify_webhook.py
@@ -0,0 +1,12 @@
+import requests
+from api.functions import get_secret
+
+def notify_webhook(data={}, webhook_url=get_secret("WEBHOOK_URL"), headers={'Content-Type': 'applications/json'}):
+    
+    return requests.post(url=webhook_url, params=data, headers=headers)
+
+data = {
+    'name': 'Perturbations sur la ligne A',
+    'priority': 'URGENT',
+    'message': 'Un retard de 15 minutes est à prévoir sur tous les trains de la ligne A en matinée.'
+}
