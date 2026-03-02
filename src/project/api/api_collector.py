6b83ea1273ecdf548dd9f59359a38adb2042f8d4
jossua-ws <jossua.thuiller@wavestone.com>
2026-03-02 10:28:21 +0000

    Added RATP data in data dir

--- src/project/api/api_collector.py
+++ src/project/api/api_collector.py
@@ -10,8 +10,15 @@ url = host + path
 
 # RER A
 # C01742
+# id > line:IDFM:C01742
 # Métro 4
 # C01374
+# id > line:IDFM:C01374
+
+# La Défense (Grande Arche) - M1
+# 
+# La DéfenseIDFM:463193 - RER A
+# IDFM:monomodalStopPlace:470549
 
 # Disruptions - line A
 url=host+"/marketplace/v2/navitia/line_reports/lines/line%3AIDFM%3AC01742/line_reports?count=100"
